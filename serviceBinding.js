75b8cd129fc81b7e04373a89fe64f8c3f1699452
Code Fabric <66582017+ronaldebch@users.noreply.github.com>
2020-06-10 12:51:32 -0400

    Update serviceBinding.js

--- serviceBinding.js
+++ serviceBinding.js
@@ -1,5 +1,5 @@
 function initModel() {
 	var sUrl = "/fabriccode/V2/Northwind/Northwind.svc/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
+	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true.);
 	sap.ui.getCore().setModel(oModel);
 }
